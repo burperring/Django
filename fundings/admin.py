dbad51a08e2a4507bccafff288f85b24efc5bfee
burperring <khw5231@gmail.com>
2022-03-23 18:39:52 +0900

    Change admin panel

--- fundings/admin.py
+++ fundings/admin.py
@@ -13,9 +13,73 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Funding)
 class FundingAdmin(admin.ModelAdmin):
 
-    """Music Admin Definition"""
+    """Funding Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "adress", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host")},
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "music_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+        "count_amenities",
+    )
+
+    search_fields = (
+        "=city",
+        "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return ""
 
 
 @admin.register(models.Photo)
