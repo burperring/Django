6bcf48ddaaebfb482c77737ba08f8a26da36070e
burperring <khw5231@gmail.com>
2022-03-23 17:04:56 +0900

    Create musics panel

--- musics/admin.py
+++ musics/admin.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.MusicType)
+class ItemAdmin(admin.ModelAdmin):
+
+    """Item Admin Definition"""
+
+    pass
+
+
 @admin.register(models.Music)
 class MusicAdmin(admin.ModelAdmin):
 
     """Music Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """"""
 
     pass
