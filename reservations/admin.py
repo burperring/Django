413285a75fc1012f6ae3119a8fd948c326eeae44
burperring <khw5231@gmail.com>
2022-03-29 17:19:59 +0900

    Add reservations display

--- reservations/admin.py
+++ reservations/admin.py
@@ -7,4 +7,17 @@ class ReservationAdmin(admin.ModelAdmin):
 
     """Reservation Admin Definition"""
 
-    pass
+    list_display = (
+        "funding",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "is_finished",
+    )
+
+    list_filter = (
+        "status",
+        "in_progress",
+    )
