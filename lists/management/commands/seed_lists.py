91c2eea71bf56ab79e351d8f249c4cd817375fc8
burperring <khw5231@gmail.com>
2022-04-07 13:18:00 +0900

    Create lists seed

---
+++ lists/management/commands/seed_lists.py
@@ -0,0 +1,40 @@
+import random
+from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
+from django_seed import Seed
+from lists import models as list_models
+from users import models as user_models
+from fundings import models as funding_models
+
+
+NAME = "lists"
+
+
+class Command(BaseCommand):
+
+    help = "This command create {NAME}"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", default=2, type=int, help="How many {NAME} you want to create"
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number")
+        seeder = Seed.seeder()
+        users = user_models.User.objects.all()
+        fundings = funding_models.Funding.objects.all()
+        seeder.add_entity(
+            list_models.List,
+            number,
+            {
+                "user": lambda x: random.choice(users),
+            },
+        )
+        created = seeder.execute()
+        cleaned = flatten(list(created.values()))
+        for pk in cleaned:
+            list_model = list_models.List.objects.get(pk=pk)
+            to_add = fundings[random.randint(0, 5) : random.randint(6, 30)]
+            list_model.fundings.add(*to_add)
+        self.stdout.write(self.style.SUCCESS(f"{number} {NAME} created!"))
