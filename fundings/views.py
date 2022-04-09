ff663b65d8c972028892147d8adb8d181fd498d9
burperring <khw5231@gmail.com>
2022-04-09 15:32:10 +0900

    Add funding home page

--- fundings/views.py
+++ fundings/views.py
@@ -3,7 +3,11 @@ from . import models
 
 
 def all_fundings(request):
-    all_fundings = models.Funding.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_fundings = models.Funding.objects.all()[offset:limit]
     return render(
         request, "fundings/all_fundings.html", context={"fundings": all_fundings}
     )
