b7e191bf8565dc170fbad0ae646969ee884d7609
burperring <khw5231@gmail.com>
2022-04-09 17:04:13 +0900

    Add exception

--- fundings/views.py
+++ fundings/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
 def all_fundings(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     funding_list = models.Funding.objects.all()
-    paginator = Paginator(funding_list, 10)
-    fundings = paginator.get_page(page)
-    return render(
-        request,
-        "fundings/all_fundings.html",
-        {"fundings": fundings},
-    )
+    paginator = Paginator(funding_list, 10, orphans=3)
+    try:
+        fundings = paginator.page(int(page))
+        return render(
+            request,
+            "fundings/all_fundings.html",
+            {"page": fundings},
+        )
+    except EmptyPage:
+        fundings = paginator.page(1)
+        return redirect("/")
